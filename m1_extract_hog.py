9de4b658c5bfeb56f034df9597b8f341f02f97fe
Roman Stanchak <rstanchak@semaconnect.com>
2017-05-23 22:08:14 -0400

    code for extracting HoG features

--- m1_extract_hog.py
+++ m1_extract_hog.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python
 
-from skimage.feature import hog as skhog
 import numpy as np
 import cv2
 import sys
 import glob
 import os
-
-def hog( infname, outfname):
-    img = cv2.imread(infname)
-    luv = cv2.cvtColor(img, cv2.COLOR_BGR2LUV)
-    hog_arr = []
-    for ch in range(3):
-        features = skhog(luv[:,:,ch], orientations=9, pixels_per_cell=(8, 8),
-                cells_per_block=(2, 2), feature_vector=False)
-        assert(np.isfinite(features).all())
-        hog_arr.append(features)
-    np.save(outfname, np.asarray(hog_arr))
+import json
+from extract_features import HogFeatureExtractor
 
 def find_samples( subdir ):
-    return sorted(glob.glob(os.sep.join([subdir, '**', '*.jpg']), recursive=True))
+    return sorted(glob.glob(os.sep.join([subdir, '**', '*.png']), recursive=True) 
+            + glob.glob(os.sep.join([subdir, '**', '*.jpg']), recursive=True))
+
+def usage():
+    print ("USAGE: {} <env fname> <input directory> <output directory>")
 
 if __name__=="__main__":
     assert(len(sys.argv)>2)
-    image_dir = sys.argv[1]
+    with open(sys.argv[1]) as f:
+        env = json.load(f)
+    image_dir = sys.argv[2]
+    hog = HogFeatureExtractor( cspace=env['colorspace'], orient=env['orient'], pix_per_cell=env['pix_per_cell'], cell_per_block=env['cell_per_block'], hog_channel=env['hog_channel'] )
     samples = find_samples( image_dir )
     print("Found {} samples".format(len(samples)))
     for infname in samples:
         outfname = os.path.sep.join([sys.argv[2], infname[(len(image_dir)+1):].replace(os.path.sep,'__')]) + '.npy'
-        print("hog {} {}".format(infname, outfname))
-        hog(infname, outfname)
+        print("hog {} {} ({})".format(infname, outfname, json.dumps(env)))
+        img = cv2.imread(infname)
+        features = hog.preprocess(img)
+        np.save(outfname, features)
     print("OK")
