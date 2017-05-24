fa4e028cd89eea303334ff46be1df991a2f683fb
Roman Stanchak <rstanchak@semaconnect.com>
2017-05-23 22:23:12 -0400

    training script

--- m2_train.py
+++ m2_train.py
@@ -7,9 +7,11 @@ import numpy as np
 import numpy.random as nprand
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+
 from sklearn.externals import joblib
 import sklearn.svm as svm
+from sklearn.metrics import classification_report
 
 def usage(progname):
     print("USAGE: %s <data directory>" % progname)
@@ -20,10 +22,10 @@ if __name__=='__main__':
     features_array=[]
     labels_array=[]
     print("Loading {} samples...".format(len(fileset)))
-    N = 5000
+    N = -1
     idx = list(range( len(fileset) ))
     nprand.shuffle(idx)
-    #idx = idx[:N]
+    idx = idx[:N]
     for fname in map(lambda x: fileset[x], idx):
         features = np.load(fname)
         features_array.append( features.ravel() )
@@ -33,10 +35,12 @@ if __name__=='__main__':
     labels_idx = dict( map( reversed, enumerate( set(labels_array))))
     y = np.asarray( list(map( labels_idx.get, labels_array ) ))
 
+    X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.33)
     print("Training")
-    cv = 2
     clf = make_pipeline( StandardScaler(), svm.SVC(C=1,verbose=True))
-    scores = cross_val_score(clf, X, y, cv=cv)
-    print(scores)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print(classification_report(y_test, y_pred, target_names=list(labels_idx.keys())))
     joblib.dump( clf, 'P5-clf.pk' )
 
